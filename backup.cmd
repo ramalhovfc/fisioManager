fa74c98ac17fca7cf7a0f7a71a223fb04612b3fb
ramalhovfc <ramalho.vfc@gmail.com>
2017-02-20 00:20:46 +0000

    Update backup script to export to file with timestamp

--- backup.cmd
+++ backup.cmd
@@ -28,6 +28,13 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set fullstamp=%YYYY%-%MM%-%DD%_%HH%%Min%-%Sec%
+
 sc stop MongoDB
-7z a c:\dev\archive.zip c:\data\db
+7z a %fullstamp%.zip c:\data\db
 sc start MongoDB
